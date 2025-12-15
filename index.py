c464396c0892607e1a3f7b29108b41bf18d36253
BRAYAN-DESIGN <kamgaingbrayan7@gmail.com>
2025-12-15 19:21:52 +0100

    first commit

--- index.py
+++ index.py
@@ -1,16 +1,1 @@
-class Vehicule:
-    def move(self):
-        return "Le véhicule se déplace"
-
-class Voiture(Vehicule):
-    def __init__(self, marque, modele):
-        self.marque = marque
-        self.modele = modele
-
-    def accelerer(self):
-        print(f"{self.marque} {self.modele} accélère.")
-
-mercedes = Voiture("Mercedes", "Classe A")
-print(mercedes.move())
-mercedes.accelerer()
-# ...existing code...
+print('hello GITHUB')
